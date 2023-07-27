77076f908b85547a7be43bd2a30507922455bbce
unknown <guruvihar2022@gmail.com>
2023-07-27 15:56:50 +0530

    initial commit

---
+++ src/main/java/com/verizon/cloud/gateway/controller/FallBackMethodController.java
@@ -0,0 +1,18 @@
+package com.verizon.cloud.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+	@GetMapping("/medicineServiceFallBack")
+	public String medicineSerivceFallBackMethod() {
+		return "Medicine service fall back method";
+	}
+	
+	@GetMapping("/invoiceServiceFallBack")
+	public String invoiceServiceFallBackMethod() {
+		return "Invoice service fall back method";
+	}
+}
